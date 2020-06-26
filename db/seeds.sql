dda50afaf0f17d3accd6c83478e870f47b6608ac
Sammy Sloan <smileysammy3@gmail.com>
2020-06-26 14:00:25 -0600

    added database details

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Baby You Can Chive My Car');
+INSERT INTO burgers (burger_name) VALUES ('Beet-er Late Than Never');
+INSERT INTO burgers (burger_name) VALUES ('Dont You For Cheddar `Bout Me');
