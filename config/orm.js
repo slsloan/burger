f12abc35f125a9ccf6d39dedf5efd9c7987cbc2f
Sammy Sloan <smileysammy3@gmail.com>
2020-06-28 21:41:40 -0600

    added the rest of the crud functions

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,6 @@ var connection = require("../config/connection.js");
 
 function printQuestionMarks(num) {
     var arr = [];
-
     for (var i = 0; i < num; i++) {
         arr.push("?");
     }
@@ -11,10 +10,8 @@ function printQuestionMarks(num) {
 
 function objToSql(ob) {
     var arr = [];
-
     for (var key in ob) {
         var value = ob[key];
-
         if (Object.hasOwnProperty.call(ob, key)) {
 
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
@@ -23,24 +20,25 @@ function objToSql(ob) {
             arr.push(key + "=" + value);
         }
     }
+
     return arr.toString();
 }
 
 var orm = {
-    selectAll: function (tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
+    // Display all burgers in the db.
+    selectAll: function (table, cb) {
+        var queryString = "SELECT * FROM " + table + ";";
 
         connection.query(queryString, function (err, result) {
             if (err) {
                 throw err;
             }
-
             cb(result);
         });
     },
+    // Add a burger to the db.
     insertOne: function (table, cols, vals, cb) {
         var queryString = "INSERT INTO " + table;
-
         queryString += " (";
         queryString += cols.toString();
         queryString += ") ";
@@ -48,27 +46,44 @@ var orm = {
         queryString += printQuestionMarks(vals.length);
         queryString += ") ";
 
+        console.log(queryString);
+
         connection.query(queryString, vals, function (err, result) {
             if (err) {
-                throw err;
+                throw err
             }
-
             cb(result);
         });
     },
+    // Set burger devoured status to true.
     updateOne: function (table, objColVals, condition, cb) {
         var queryString = "UPDATE " + table;
-
         queryString += " SET ";
         queryString += objToSql(objColVals);
         queryString += " WHERE ";
         queryString += condition;
 
+        console.log(queryString);
+
         connection.query(queryString, function (err, result) {
             if (err) {
-                throw err;
+                throw err
             }
+            cb(result);
+        });
+    },
+    // Delete a burger from the db.
+    deleteOne: function (table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
 
+        console.log(queryString);
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err
+            }
             cb(result);
         });
     }
